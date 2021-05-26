8b25ec708543dba0f446668c817bed1859635fa9
Marie Burki <marie.buerki@gmail.com>
2021-05-26 15:10:05 +0200

    basic display main, aside, article : needs polishing

--- interface.js
+++ interface.js
@@ -1,15 +1,27 @@
 // DOM elements
 
+// header
 const headerH2 = document.querySelector(".logo-h2");
 const menu = document.querySelector(".nav-menu");
 const menuBtn = document.querySelector(".nav-btn");
 const line2 = document.querySelector(".line2");
 const line3 = document.querySelector(".line3");
 
+// articles
+const article = document.querySelector(".article");
+const aside = document.querySelector("aside");
+const asideBtn = document.querySelector(".btn-aside");
+const paragraphBtns = document.querySelectorAll(".btn-show-text");
+const how = document.querySelector("#how");
+const howMuch = document.querySelector("#how-much");
+const when = document.querySelector("#when");
+const restrictions = document.querySelector("#restrictions");
+
+
 // Open-close navigation menu
 
 function openNav() {
-    headerH2.style.marginBottom = "28px";
+    headerH2.style.marginBottom = "45px";
     line2.style.width = "24px";
     line3.style.width = "29px";
     menu.id = "opened-menu";
@@ -32,4 +44,26 @@ menuBtn.addEventListener("click", ()=> {
         closeNav();
     }
     menuBtn.setAttribute("aria-expanded", x);
+});
+
+// Article : display/hide paragraphs
+
+function displayText(element) {
+    element.classList.toggle("hidden-p");
+}
+
+paragraphBtns.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+        btn.classList.toggle("btn-show-text-clicked");
+        document.activeElement.blur();
+    });
+});
+
+// Aside : display/hide navigation
+
+asideBtn.addEventListener("click", ()=>{
+    asideBtn.classList.toggle("btn-aside-clicked");
+    aside.classList.toggle("article-nav-closed");
+    article.classList.toggle("article-narrow");
+    document.activeElement.blur();
 });
--- recipes.js
+++ recipes.js
@@ -1,11 +1,12 @@
 // fetch data
 
-fetch('recipes.json')
+fetch('https://raw.githubusercontent.com/marie-bu/EcoArava/f5393f81915e625816d7293b97e24d94cf0d1cb3/recipes.json')
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        appendDataIndex(data);
+        appendDataGrid(data);
+        appendDataLightbox(data);
     })
     .catch(function(error) {
         console.log(error);
@@ -13,12 +14,101 @@ fetch('recipes.json')
 
 // DOM elements
 
-const tag = document.querySelectorAll(".tag");
+const recipesGrid = document.querySelector(".recipes-grid");
+const recipesLightbox = document.querySelector(".recipes-details");
+const recipesInstructions = document.querySelector(".recipes-instructions");
+const closeLightboxBtn = document.querySelector(".btn-close");
+const tags = document.querySelectorAll(".tag");
+
+// HTML
+
+function HTMLgrid(object, i) {
+    recipesGrid.innerHTML += `
+        <figure class="recipes-grid-item" onclick="openLightbox();showSlide(`+[i]+`)">
+            <img src="recipes/`+object[i].image+`" alt="">
+            <figcaption><em class="centered">`+object[i].name+`</em></figcaption>
+        </figure>`
+};
+
+function HTMLlightbox(object, i) {
+    recipesLightbox.innerHTML += `
+    <div class="recipes-details-item">
+      <img src="recipes/`+object[i].image+`" alt="">
+      <div class="item-ingredients">
+        <p class="bold">Ingredients</p>
+        <ul class="content-left-ingredients"></ul>
+      </div>
+      <h3 class="item-title">`+object[i].name+`</h3>
+      <p class="item-level"><span class="bold">Level :</span> `+object[i].level+`</p>
+      <p class="item-directions"><span class="bold">Directions :</span> `+object[i].directions+`</p>
+</div>`
+}
 
 // Tag and filter
 
-tag.forEach(tag => tag.addEventListener("click", function(tag) {
-    tag.target.style.backgoundColor = "$color-quaternary";
+tags.forEach(tag => tag.addEventListener("click", function(e) {
+    let clickedTag = e.target;
+    let x = clickedTag.getAttribute("aria-pressed");
+    if (x == "false") {
+        x = "true";
+        clickedTag.classList.add("tag-selected");
+    } else {
+        x = "false";
+        clickedTag.classList.remove("tag-selected");
+        document.activeElement.blur();
+    }
+    clickedTag.setAttribute("aria-pressed", x);
 }));
 
+function appendDataGrid(data) {
+    for (let i=1; i<data.recipes.length; i++) {
+        HTMLgrid(data.recipes, i);
+    }
+};
 
+function appendDataLightbox(data) {
+    for (let i=1; i<data.recipes.length; i++) {
+        HTMLlightbox(data.recipes, i);
+
+        const ingredients = document.querySelectorAll(".item-ingredients");
+
+    Array.from(data.recipes[i].ingredients).forEach((ingredient)=>{
+      ingredients[i].innerHTML +=
+          `<span class="item-ingredient">`+ingredient.ingredient+`:</span>
+          <span class="item-quantity"> `+ingredient.quantity+`</span></br>`
+  })
+    }
+};
+
+// Lightbox : open, close, show clicked recipe, navigate recipes with keyboard
+
+function openLightbox() {
+  recipesLightbox.style.display = "block";
+  recipesLightbox.setAttribute("tabindex", "0");
+  recipesLightbox.focus();
+
+  recipesGrid.style.display = "none";
+  recipesInstructions.style.display = "none";
+}
+
+function closeLightbox(){
+  recipesLightbox.style.display = null;
+  recipesGrid.style.display = null;
+  recipesInstructions.style = null;
+}
+
+  /*function openLbKeyboard(event, element){
+    if(event.key=="Enter"){
+      openLightbox(element)
+    }
+  }*/
+  
+  function showSlide(n){
+    const mediasArray = document.querySelectorAll(".recipes-display-details-item");
+
+    for (i=0; i<mediasArray.length; i++) {
+      mediasArray[i].style.display = "none";
+    }
+
+    mediasArray[n].style.display = "grid";
+  }
